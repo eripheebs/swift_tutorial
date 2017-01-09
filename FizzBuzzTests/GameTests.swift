b6747b4e5d7383fd9e538436e2aa72d972538e3c
Pheby , Erika (UK - London) <epheby@uk160106.local>
2017-01-09 12:02:27 +0000

    test

---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Pheby, Erika (UK - London) on 09/01/2017.
+//  Copyright © 2017 Pheby, Erika (UK - London). All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func thisIsTrue(){
+        XCTAssertEqual(true, true)
+    }
+    
+    func theGameStartsAtZero(){
+        XCTAssertTrue(game.score == 0)
+    }
+    
+}
